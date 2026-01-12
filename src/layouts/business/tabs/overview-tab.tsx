a04b11a869e2cd600b52580a814ec44eccfba9c5
davidcodes17 <areegbedavid@gmail.com>
2026-01-12 19:47:45 +0100

    Refactor code structure for improved readability and maintainability

--- src/layouts/business/tabs/overview-tab.tsx
+++ src/layouts/business/tabs/overview-tab.tsx
@@ -5,9 +5,9 @@ const OverviewTab = () => {
     <div className="">
       <div className="flex pt-3 gap-2">
         <FilterCompo data={"ALL TIME"} />
-        <FilterCompo data={"ALL TIME"} />
-        <FilterCompo data={"ALL TIME"} />
-        <FilterCompo data={"ALL TIME"} />
+        <FilterCompo data={"ALL STATUS"} />
+        <FilterCompo data={"ALL INDUSTRIES"} />
+        <FilterCompo data={"ALL LOCATIONS"} />
       </div>
     </div>
   );
